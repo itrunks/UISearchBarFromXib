240e2a8389303db70406d831af4e51a8ad882822
iDevAndroid <rajapitchai.p@gmail.com>
2014-01-18 12:13:11 +0530

    Initial Commit

---
+++ UISearchBarFromXib/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  UISearchBarFromXib
+//
+//  Created by Fobess on 1/18/14.
+//  Copyright (c) 2014 Fobess. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ UISearchBarFromXibTests/UISearchBarFromXibTests.m
@@ -0,0 +1,34 @@
+//
+//  UISearchBarFromXibTests.m
+//  UISearchBarFromXibTests
+//
+//  Created by Fobess on 1/18/14.
+//  Copyright (c) 2014 Fobess. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface UISearchBarFromXibTests : XCTestCase
+
+@end
+
+@implementation UISearchBarFromXibTests
+
+- (void)setUp
+{
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown
+{
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    XCTFail(@"No implementation for \"%s\"", __PRETTY_FUNCTION__);
+}
+
+@end
